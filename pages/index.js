fa3afc408034840cb86e7913ca7a1e1aa72043d2
jasalguero <jose@aplos.com>
2021-11-18 23:41:12 +0000

    added google sign in

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,9 @@ export default function Index() {
       <button onClick={(e) => auth.signout()}>Sign Out</button>
     </div>
   ) : (
-    <button onClick={(e) => auth.signinWithGitHub()}>Sign In</button>
+    <>
+    <button onClick={(e) => auth.signinWithGitHub()}>Sign In with Github</button>
+    <button onClick={(e) => auth.signinWithGoogle()}>Sign In with Google</button>
+    </>
   )
 }
