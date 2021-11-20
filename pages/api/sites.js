782a81c4d75e054952eb2629f9b95cbc3dd70549
jasalguero <jose@aplos.com>
2021-11-20 23:15:11 +0000

    added dashboard sites page

---
+++ pages/api/sites.js
@@ -0,0 +1,13 @@
+import { getAllSites } from "@/lib/db-admin";
+
+const queryGetAllSites = async (_, res) => {
+  const result = await getAllSites();
+
+  if (result.error) {
+    res.status(500).json({ error: result.error });
+  }
+
+  res.status(200).json({ sites: result.sites });
+};
+
+export default queryGetAllSites;
