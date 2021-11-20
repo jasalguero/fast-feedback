2b4979c8e6f9f26065011446311c3e96826b0d5c
jasalguero <jose@aplos.com>
2021-11-20 00:33:00 +0000

    added feedback creation

--- pages/p/[siteId].js
+++ pages/p/[siteId].js
@@ -1,12 +1,14 @@
+import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
-import { Box } from "@chakra-ui/react";
+import { Box, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
 import { getAllFeedback, getAllSites } from "@/lib/db-admin";
+import { createFeedback } from "@/lib/db";
+import Feedback from "@/components/Feedback";
+import { useAuth } from "@/lib/auth";
 
 export async function getStaticProps(context) {
   const siteId = context.params.siteId;
-  console.log("siteId", siteId);
   const { feedback } = await getAllFeedback(siteId);
-  console.log("feedback", feedback);
   return {
     props: {
       initialFeedback: feedback,
@@ -27,13 +29,69 @@ export async function getStaticPaths() {
     paths,
     fallback: true,
   };
-
 }
 
 const FeedbackPage = ({ initialFeedback }) => {
+  const auth = useAuth();
   const router = useRouter();
+  const inputEl = useRef(null);
+  const [allFeedback, setAllFeedback] = useState([]);
 
-  return <Box>Site ID: {router.query.siteId}</Box>;
+  useEffect(() => {
+    setAllFeedback(initialFeedback);
+  }, [initialFeedback]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newFeedback = {
+      author: auth.user.name,
+      authorId: auth.user.uid,
+      siteId: router.query.siteId,
+      text: inputEl.current.value,
+      createdAt: new Date().toISOString(),
+      provider: auth.user.provider,
+      status: "pending",
+    };
+
+    inputEl.current.value = "";
+    createFeedback(newFeedback).then((docRef) => {
+      setAllFeedback((currentFeedback) => [
+        {
+          ...newFeedback,
+          id: docRef.id,
+        },
+        ...currentFeedback,
+      ]);
+    });
+  };
+
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      width="full"
+      maxWidth="700px"
+      margin="0 auto"
+    >
+      {auth.user && (
+        <Box as="form" onSubmit={onSubmit}>
+          <FormControl my={8}>
+            <FormLabel htmlFor="comment">Comment</FormLabel>
+            <Input ref={inputEl} id="comment" placeholder="Leave a comment" />
+            <Button mt={4} type="submit" fontWeight="medium">
+              Add Comment
+            </Button>
+          </FormControl>
+        </Box>
+      )}
+      {/* Only render the feedback if it exists */}
+      {allFeedback &&
+        allFeedback.map((feedback) => (
+          <Feedback key={feedback.id} {...feedback} />
+        ))}
+    </Box>
+  );
 };
 
 export default FeedbackPage;
